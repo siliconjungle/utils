90aceec40556211a4e46bcfc52a3fa121751d58b
James Addison <71357672+siliconjungle@users.noreply.github.com>
2022-11-30 12:03:44 +1100

    add heartbeat and perf methods.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { performance } from 'perf_hooks'
+
 export const deepCopy = data => JSON.parse(JSON.stringify(data))
 export const deepCompare = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max)
@@ -33,4 +35,19 @@ export const throttle = (cb, delay = 1000) => {
     shouldWait = true
     setTimeout(timeoutFunc, delay)
   }
+}
+
+export const perf = (fn, name) => {
+  const start = performance.now()
+  fn()
+  const end = performance.now()
+  const ms = end - start
+  console.log(`${name || 'Function call'}: ${ms}ms`)
+}
+
+export const heartbeat = (fn, delay) => {
+  fn()
+  setTimeout(() => {
+    heartbeat(fn, delay)
+  }, delay)
 }
